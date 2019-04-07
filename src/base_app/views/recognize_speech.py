1be8835e8ad8cf7e50f413a2491391487c83baa9
obqq <nouveaudupasse@gmail.com>
2019-04-07 16:39:29 +0200

    Add integration to RecognizeSpeech view

--- src/base_app/views/recognize_speech.py
+++ src/base_app/views/recognize_speech.py
@@ -2,6 +2,7 @@ import logging
 from django.http import JsonResponse
 
 from base_app.utils.json import Json
+from base_app.integrations.speech_to_text import GoogleSpeechToTextAPI
 
 logger = logging.getLogger(__name__)
 
@@ -9,8 +10,6 @@ logger = logging.getLogger(__name__)
 class RecognizeSpeech(Json):
 
 	def post(self, request):
-
-
 		request_data = request.POST
 		audiofile = request.FILES.get('file')
 
@@ -20,7 +19,21 @@ class RecognizeSpeech(Json):
 
 
 		if audiofile is not None:
-			pass
+			API = GoogleSpeechToTextAPI()
+			response = API.recognize(audiofile, encoding, sampleRateHertz, languageCode)
+
+			error = response.get('error')
+			if error is not None:
+				return JsonResponse(
+					{'message': error.get('message')},
+					status=400
+				)
+
+			results = response.get('results')
+
+			return dict(
+				transcript='\n'.join([result['alternatives'][0]['transcript'] for result in results])
+			)
 
 		return JsonResponse(
 				{'message': 'Audio file was not specified.'},
