6c6a80001b0daec760fea9acd7262f949adbecb1
obqq <nouveaudupasse@gmail.com>
2019-04-14 20:33:22 +0200

    Add AccessToken and RefreshToken views

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,5 +5,10 @@ from api.views import UserViewSet
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
+urlpatterns = [
+    path(r'access_token', AccessToken.as_view()),
+    path(r'refresh_token', RefreshToken.as_view()),
+    path(r'public_key', PublicKey.as_view())
+]
 
-urlpatterns = router.urls
+urlpatterns += router.urls
