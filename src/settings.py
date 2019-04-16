a390218edb0388b343cd06cf44bfb775679738a8
obqq <nouveaudupasse@gmail.com>
2019-04-16 21:58:57 +0200

    add rest-framework pagination settings

--- src/settings.py
+++ src/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'api'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
