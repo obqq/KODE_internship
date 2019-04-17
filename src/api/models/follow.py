35990fb742f662df0ec7b7d39af7ee03d9aadcbf
obqq <nouveaudupasse@gmail.com>
2019-04-17 17:01:18 +0200

    fix model fields related-names

--- src/api/models/follow.py
+++ src/api/models/follow.py
@@ -2,5 +2,5 @@ from django.db import models
 from .user import User
 
 class Follow(models.Model):
-    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
-    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
+    target = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name='targets', on_delete=models.CASCADE)
