3ce2339dae2fd9db7ac9aded45879f83181bb200
obqq <nouveaudupasse@gmail.com>
2019-04-21 18:38:12 +0200

    add user feed and pitts urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,15 +1,18 @@
+
 from django.urls import path
 
-from api.views import (UserListView, UserCreateView, UserViews, FollowViews,
+from api.views import (UserListView, CreateUserView, UserViewSet,
+                       FollowViews, PittListView, PittViewSet,
                        AccessTokenView, RefreshTokenView, PublicKeyView)
 
 urlpatterns = [
     path(r'users', UserListView.as_view()),
-    path(r'users/create', UserCreateView.as_view()),
-    path(r'users/<str:username>/', UserViews.as_view()),
-    path(r'users/<str:username>/follow', FollowViews.as_view()),
+    path(r'users/create', CreateUserView.as_view()),
+    path(r'users/<str:username>/', UserViewSet.as_view()),
+    path(r'users/<str:username>/follows', FollowViews.as_view()),
+    path(r'users/<str:username>/feed', PittListView.as_view()),
+    path(r'users/<str:username>/pitts', PittViewSet.as_view()),
     path(r'auth/access_token', AccessTokenView.as_view()),
     path(r'auth/refresh_token', RefreshTokenView.as_view()),
     path(r'auth/public_key', PublicKeyView.as_view())
 ]
-
